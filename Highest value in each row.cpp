36876cdaaa33987a10459ab224c46046a70be943
mselle14 <mselle14@aum.edu>
2026-04-20 15:56:17 -0500

    Update Highest value in each row.cpp

--- Highest value in each row.cpp
+++ Highest value in each row.cpp
@@ -1,20 +1,33 @@
-// Highest value in each row.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+//Marshon Sellers
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    const int STUDENTS = 3;
+    const int SUBJECTS = 4;
+    int marks[STUDENTS][SUBJECTS];
+
+    for (int row = 0; row < STUDENTS; row++)
+    {
+        for (int col = 0; col < SUBJECTS; col++)
+        {
+            cin >> marks[row][col];
+        }
+    }
+
+    for (int row = 0; row < STUDENTS; row++)
+    {
+        int max_value = marks[row][0];
+        for (int col = 1; col < SUBJECTS; col++)
+        {
+            if (marks[row][col] > max_value)
+            {
+                max_value = marks[row][col];
+            }
+        }
+        cout << "Student " << (row + 1) << " highest = " << max_value << endl;
+    }
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
