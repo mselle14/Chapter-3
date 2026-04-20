3cdf3dbfbe2872eda53ca436cc9176c530999e7a
mselle14 <mselle14@aum.edu>
2026-04-20 15:55:04 -0500

    Update daily temp table.cpp

--- daily temp table.cpp
+++ daily temp table.cpp
@@ -1,20 +1,34 @@
-// daily temp table.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+//Marshon Sellers
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    const int CITIES = 3;
+    const int DAYS = 4;
+    int temp[CITIES][DAYS];
+
+    cout << "Enter temperatures for 3 cities over 4 days:\n";
+
+    for (int row = 0; row < CITIES; row++)
+    {
+        cout << "City " << (row + 1) << ":\n";
+        for (int col = 0; col < DAYS; col++)
+        {
+            cout << "  Day " << (col + 1) << ": ";
+            cin >> temp[row][col];
+        }
+    }
+
+    cout << "\nTemperature Table:\n";
+    for (int row = 0; row < CITIES; row++)
+    {
+        for (int col = 0; col < DAYS; col++)
+        {
+            cout << temp[row][col] << "\t";
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
